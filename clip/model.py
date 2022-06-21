c877b8a734fc7faed2b790cdd4eb6dfdb3b1536e
DhruvBhardwaj <DhruvBhardwaj@users.noreply.github.com>
2022-06-21 22:46:52 +0530

    Removed posn embedding from both image and text

--- clip/model.py
+++ clip/model.py
@@ -68,7 +68,7 @@ class AttentionPool2d(nn.Module):
     def forward(self, x):
         x = x.reshape(x.shape[0], x.shape[1], x.shape[2] * x.shape[3]).permute(2, 0, 1)  # NCHW -> (HW)NC
         x = torch.cat([x.mean(dim=0, keepdim=True), x], dim=0)  # (HW+1)NC
-        #x = x + self.positional_embedding[:, None, :].to(x.dtype)  # (HW+1)NC
+        # x = x + self.positional_embedding[:, None, :].to(x.dtype)  # (HW+1)NC
         x, _ = F.multi_head_attention_forward(
             query=x, key=x, value=x,
             embed_dim_to_check=x.shape[-1],
@@ -344,7 +344,7 @@ class CLIP(nn.Module):
     def encode_text(self, text):
         x = self.token_embedding(text).type(self.dtype)  # [batch_size, n_ctx, d_model]
 
-        x = x + self.positional_embedding.type(self.dtype)
+        # x = x + self.positional_embedding.type(self.dtype)
         x = x.permute(1, 0, 2)  # NLD -> LND
         x = self.transformer(x)
         x = x.permute(1, 0, 2)  # LND -> NLD
